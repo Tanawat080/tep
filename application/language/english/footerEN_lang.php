1bf9901709f125be8cb2ea7a3cbb3771ab48ade6
EMAILPSU\5730213036 <saranyuu88@gmail.com>
2017-11-09 17:48:32 +0700

    no message

---
+++ application/language/english/footerEN_lang.php
@@ -0,0 +1,42 @@
+<?php 
+//footer
+	$lang['Service']='Service';
+	$lang['PaymentGatewaySystem']='Payment Gateway System';
+	$lang['ShoppingCartSystem']='Shopping Cart System';
+	$lang['BookingSystem']='Booking System';
+	
+
+	$lang['LatestNewsPromotion']='Latest News & Promotion';
+	$lang['SMSOrderAlertTemporarilyClosed']='SMS Order Alert Temporarily Closed';
+	$lang['contentSMS']='e to inform you that our SMS Order Alert is now temporarily closed for system maintenance. Once the system is back to wor...';
+
+	$lang['OurLineOfficialAccount']='Our Line@ Official Account';
+	$lang['contentLine']='ation channel for you to connect with us via our LINE@ Official Account. You can easily add us as a Friend instantly by s...';
+
+	$lang['Subject']='Subject: Changing New Company Telephone No.';
+	$lang['contentSubject']='We, Pay Solutions Co. Ltd., would like to inform you that our new company telephone no. has been changed to the foll...';
+
+	
+
+	$lang['HotArticles']='Hot Articles';
+	$lang['KnowledgeBase']='Knowledge Base';
+	$lang['contentKnowledge']='Basic information about our services, both Payment system and Shop system';
+	$lang['TipsandTechnics']='Tips and Technics';
+	$lang['contentTip']='Tips and techniques of how to use our systems and special tricks';
+	$lang['FAQ']='FAQ';
+	$lang['contentFAQ']='General questions and problems asked by merchants, sellers and buyers';
+	$lang['Download Documents & Modules']='Download Documents & Modules';
+	$lang['contentDownload']='Download document files and related modules with instructions';
+	$lang['Others']='Others';
+	$lang['contentOthers']='Articles and other related details';
+	
+
+
+
+	$lang['WhyThaiePay']='Why ThaiePay?';
+	$lang['Paysolutions']='Paysolutions.co.,Ltd.';
+	$lang['Address']='522 Ratchadapisek 26, Ratchadapisek Rd., Samsennok, Huai Khwang Bangkok 10310, Thailand ';	
+	$lang['tel']='tel';
+	$lang['Email']='Email';
+
+?>
---
+++ application/language/english/homepage_lang.php
@@ -0,0 +1,32 @@
+<?php
+	$lang['Shopping Mall']='Our Shopping Mall service is suitable for users who want a complete e-commerce system. This service consists of all e-commerce functions such as online shop, shopping cart system, online payment system via credit card. You only have ideas and products for selling online. And then, you can open online shop with us instantly.';
+	$lang['OnlineBusinesswithOnlinePayment']='Online Business with Online Payment';
+	$lang['OnlineShop']='Online Shop';
+	$lang['Free']='Free.!';
+	$lang['OnlineReservation']='Online Reservation';
+	$lang['Readywebsiteforbusinessandcompany']='Ready website for business and company';
+	$lang['Learnmore']='Learn more';
+
+	
+
+	$lang['eModules']='Our eModules service is suitable for user that already has a website but is looking for online payment system or booking system to fulfill the business.eModules is divided according to users’ needs and business types.';
+	$lang['PaymentGatewaySystem']='Payment Gateway System';
+	$lang['ShoppingCartSystem']='Shopping Cart System';
+	$lang['BookingSystem']='Booking System';
+	$lang['OnlineDonationSystem']='Online Donation System';
+
+
+
+	$lang['features']='ThaiePay outstanding features when compared to other service providers as indicated below. We hope the information below help clarify you about ThaiePay service.';
+	$lang['ThaiePayisapioneerandprototype']='ThaiePay is a pioneer and prototype';
+	$lang['ThaiePayoffersbetterserviceanduser-friendlysystem']='ThaiePay offers better service and user-friendly system';
+	$lang['ThaiePayservicechargeisinexpensiveandworthusing']='ThaiePay service charge is inexpensive and worth using';
+	$lang['ThaiePayprovidesthehighestsecuritysystem']='ThaiePay provides the highest security system';
+	$lang['register']='register';
+
+
+
+
+
+
+?>
